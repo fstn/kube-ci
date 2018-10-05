52e5bb57519c3c767e2d13e4527f1af278296a57
stephen.zambaux <stephen.zambaux@gmail.com>
2018-10-05 10:01:17 +0200

    Update doc

--- kubeci/init/init.sh
+++ kubeci/init/init.sh
@@ -28,6 +28,8 @@ build()
     echo "[INIT] read deployment config files inside -> .deployments.gitlab-ci.yml for branch ${CI_COMMIT_REF_NAME}"
 
     export DNS=$(ConfigUtils.getValueFromConfig ".deployments.gitlab-ci.yml" "${CI_COMMIT_REF_NAME}.dns")
+    export DEBUG=$(ConfigUtils.getValueFromConfig ".deployments.gitlab-ci.yml" "${CI_COMMIT_REF_NAME}.debug")
+    export LOG_LEVEL=$(ConfigUtils.getValueFromConfig ".deployments.gitlab-ci.yml" "${CI_COMMIT_REF_NAME}.logLevel")
     echo "Reading DNS: ${DNS}"
 
 
